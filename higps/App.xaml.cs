3725cfec50b8dde54c8cc05b5f72da8a960c9671
Kyle Chew <kylec@digital-control.com>
2020-02-02 23:25:09 -0800

    testing

---
+++ higps/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using higps.Services;
+using higps.Views;
+
+namespace higps
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
